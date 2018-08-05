a76bf2ba05d381b56fe4c6669fa99d2784e3aab9
Brienyll <brienyll29@gmail.com>
2018-08-05 15:38:28 -0700

    cleaned code

--- exercises/palindrome.js
+++ exercises/palindrome.js
@@ -8,7 +8,11 @@
 //   palindrome("abcdefg") === false
 
 function palindrome(str) {
-    const reversed = str.split('').reverse().join('');
+    const reversed = str
+        .split('')
+        .reverse()
+        .join('');
+        
     return str === reversed;
 }
 module.exports = palindrome;
